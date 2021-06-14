274b33c13f5d0553a7d05150b982b722f8023f14
Matheus Figueiredo <ultra10vascaino@gmail.com>
2021-06-14 02:30:14 -0300

    Adicionando novas páginas de instituição.

--- src/components/instituicao/editaInstituicao.js
+++ src/components/instituicao/editaInstituicao.js
@@ -47,63 +47,137 @@ class EditaInstituicao extends Component {
             <div style={{marginTop: 20}}>
                 <h3>Editar Instituição</h3>
                 <br/>
-                <form onSubmit={this.onSubmit}>
-                    <div class="form-row">
-                        <div className="form-group col-md-3">
-                            <label>Nome: </label>
-                            <input  type="text"
-                                    className="form-control"
-                                    name='nome'
-                                    value={this.state.nome}
-                                    onChange={this.onChangeNome}
-                                    />
+                    <form onSubmit={this.onSubmit}>
+                        <div class="form-row">
+                            <div className="form-group col-md-5">
+                                <label>Nome Fantasia: </label>
+                                <input  type="text"
+                                        className="form-control"
+                                        name='nome'
+                                        value={this.state.nome}
+                                        onChange={this.onChangeNome}
+                                        />
+                            </div>
                         </div>
-                        <div className="form-group col-md-3">
-                            <label>CNPJ: </label>
-                            <input  type="text"
-                                    className="form-control"
-                                    name='cnpj'
-                                    value={this.state.cnpj}
-                                    onChange={this.onChangeCNPJ}
-                                    />
+                        <div class="form-row">
+                            <div className="form-group col-md-5">
+                                <label>Razão Social: </label>
+                                <input  type="text"
+                                        className="form-control"
+                                        name='razao'
+                                        value={this.state.razao}
+                                        onChange={this.onChangeRazao}
+                                        />
+                            </div>
                         </div>
-                    </div>
-                    <div class="form-row">
-                        <div className="form-group col-md-2">
-                            <label>CNEN: </label>
-                            <input  type="text"
-                                    className="form-control"
-                                    name='cnen'
-                                    value={this.state.cnen}
-                                    onChange={this.onChangeCnen}
-                                    />
+                        <div class="form-row">
+                            <div className="form-group col-md-2">
+                                <label>CNPJ: </label>
+                                <input  type="text"
+                                        className="form-control"
+                                        name='cnpj'
+                                        value={this.state.cnpj}
+                                        onChange={this.onChangeCNPJ}
+                                        />
+                            </div>
+                            <div className="form-group col-md-4">
+                                <label>E-mail: </label>
+                                <input  type="email"
+                                        className="form-control"
+                                        name='email'
+                                        value={this.state.email}
+                                        onChange={this.onChangeEmail}
+                                        />
+                            </div>
                         </div>
-                        <div className="form-group col-md-4">
-                            <label>E-mail: </label>
-                            <input  type="email"
-                                    className="form-control"
-                                    name='email'
-                                    value={this.state.email}
-                                    onChange={this.onChangeEmail}
-                                    />
+                        <div class="form-row">
+                            <div className="form-group col-md-2">
+                                <label>CNEN: </label>
+                                <input  type="text"
+                                        className="form-control"
+                                        name='cnen'
+                                        value={this.state.cnen}
+                                        onChange={this.onChangeCnen}
+                                        />
+                            </div>
+                            <div className="form-group col-md-3">
+                                <label>Telefone: </label>
+                                <input  type="text"
+                                        className="form-control"
+                                        name='telefone'
+                                        value={this.state.telefone}
+                                        onChange={this.onChangeTelefone}
+                                        />
+                            </div>
                         </div>
-                    </div>
-                    <div class="form-row">
-                        <div className="form-group col-md-3">
-                            <label>Telefone: </label>
-                            <input  type="text"
-                                    className="form-control"
-                                    name='telefone'
-                                    value={this.state.telefone}
-                                    onChange={this.onChangeTelefone}
-                                    />
+                        <hr /> <br />
+                        <h4>Endereço</h4>
+                        <br/>
+                        <div class="form-row">
+                            <div className="form-group col-md-2">
+                                <label>CEP: </label>
+                                <input  type="text"
+                                        className="form-control"
+                                        name='cep'
+                                        value={this.state.cep}
+                                        onChange={this.onChangeCEP}
+                                        />
+                            </div>
                         </div>
-                    </div>
-                    <br/>
-                    <div className="form-group">
-                        <input type="submit" value="Editar Instituição" className="btn btn-primary" />
-                    </div>
-                </form>
+                        <div class="form-row">
+                            <div className="form-group col-md-4">
+                                <label>Logradouro: </label>
+                                <input  type="text"
+                                        className="form-control"
+                                        name='logradouro'
+                                        value={this.state.logradouro}
+                                        onChange={this.onChangeLogradouro}
+                                        />
+                            </div>
+                            <div className="form-group col-md-2">
+                                <label>Complemento: </label>
+                                <input  type="text"
+                                        className="form-control"
+                                        name='complemento'
+                                        value={this.state.complemento}
+                                        onChange={this.onChangeComplemento}
+                                        />
+                            </div>
+                        </div>
+                        <div class="form-row">
+                            <div className="form-group col-md-3">
+                                <label>Bairro: </label>
+                                <input  type="text"
+                                        className="form-control"
+                                        name='bairro'
+                                        value={this.state.bairro}
+                                        onChange={this.onChangeBairro}
+                                        />
+                            </div>
+                            <div className="form-group col-md-2">
+                                <label>Cidade: </label>
+                                <input  type="text"
+                                        className="form-control"
+                                        name='cidade'
+                                        value={this.state.cidade}
+                                        onChange={this.onChangeCidade}
+                                        />
+                            </div>
+                            <div className="form-group col-md-1">
+                                <label>UF: </label>
+                                <input  type="text"
+                                        className="form-control"
+                                        name='uf'
+                                        value={this.state.uf}
+                                        onChange={this.onChangeUf}
+                                        />
+                            </div>
+                        </div>
+                        <br/>
+                        <div className="form-group">
+                            <input type="submit" value="Criar Instituição" className="btn btn-primary" />
+                        </div>
+                    </form>
             </div>
         )
     }
---
+++ src/components/instituicao/mostraInstituicao.js
@@ -0,0 +1,134 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+import { Link } from 'react-router-dom'
+
+import { withAuthenticationRequired } from "@auth0/auth0-react"
+import { Loading } from '../../components'
+
+class MostraInstituicao extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            instituicao: {}
+        }
+    }
+
+    componentDidMount() {
+        axios.get('https://backend-manhattan.herokuapp.com/instituicao/'+this.props.match.params.id)
+        .then(res => {
+            this.setState({
+                instituicao: res.data
+            })
+        })
+        .catch(err => {
+            console.log('Erro para mostrar uma instituição ' + err)
+        })
+    }
+
+    onDeleteClick (id) {
+        axios.delete('https://backend-manhattan.herokuapp.com/instituicao/delete/'+id)
+        .then(res => {
+            this.props.history.push("/instituicao/todos")
+        })
+        .catch( err => {
+            console.log('Erros ao deletar uma instituição '+err)
+        })
+    }
+
+    render() {
+
+        const instituicao = this.state.instituicao
+        let InstituicaoItem = <div>
+            <table className="table table-hover">
+                <tbody>
+                <tr>
+                    <td>Nome Fantasia</td>
+                    <td>{ instituicao.nome }</td>
+                </tr>
+                <tr>
+                    <td>Razão Social</td>
+                    <td>{ instituicao.razao }</td>
+                </tr>
+                <tr>
+                    <td>Email</td>
+                    <td>{ instituicao.email }</td>
+                </tr>
+                <tr>
+                    <td>CNEN</td>
+                    <td>{ instituicao.cnen }</td>
+                </tr>
+                <tr>
+                    <td>CNPJ</td>
+                    <td>{ instituicao.cnpj }</td>
+                </tr>
+                <tr>
+                    <td>Telefone</td>
+                    <td>{ instituicao.telefone }</td>
+                </tr>
+                <tr>
+                    <td>CEP</td>
+                    <td>{ instituicao.cep }</td>
+                </tr>
+                <tr>
+                    <td>Logradouro</td>
+                    <td>{ instituicao.logradouro }</td>
+                </tr>
+                <tr>
+                    <td>Complemento</td>
+                    <td>{ instituicao.complemento }</td>
+                </tr>
+                <tr>
+                    <td>Bairro</td>
+                    <td>{ instituicao.bairro }</td>
+                </tr>
+                <tr>
+                    <td>Cidade</td>
+                    <td>{ instituicao.cidade }</td>
+                </tr>
+                <tr>
+                    <td>UF</td>
+                    <td>{ instituicao.uf }</td>
+                </tr>
+                </tbody>
+            </table>
+            </div>
+
+        return (
+            <div className="ShowBookDetails">
+                <div className="container">
+                <div className="row">
+                    <div className="col-md-10 m-auto">
+                    <br /> <br />
+                    </div>
+                    <br />
+                    <div className="col-md-8 m-auto">
+                    <h1 className="display-4 text-center">Dados da instituição</h1>
+                    <hr /> <br />
+                    </div>
+                </div>
+                <div>
+                    { InstituicaoItem }
+                </div>
+
+                <div className="row">
+                    <div className="col-md-6">
+                    <button type="button" className="btn btn-outline-danger btn-lg btn-block" onClick={this.onDeleteClick.bind(this,instituicao._id)}>Apagar instituição</button><br />
+                    </div>
+
+                    <div className="col-md-6">
+                    <Link to={`/instituicao/editar/${instituicao._id}`} className="btn btn-outline-info btn-lg btn-block">
+                            Editar instituição
+                    </Link>
+                    <br />
+                    </div>
+                </div>
+                </div>
+            </div>
+        )
+    }
+
+}
+
+export default withAuthenticationRequired(MostraInstituicao, {
+    onRedirecting: () => <Loading />,
+})
